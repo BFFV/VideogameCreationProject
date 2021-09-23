c00711eba1749b2bd901c6a3e406f49a77cb0c3d
fpattillo <fpattillo@uc.cl>
2021-09-23 15:56:21 -0300

    :sparkles: Added two enemy types

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,11 +8,23 @@ public class Player : MonoBehaviour {
 
     public float speed;
 
+    public int health;
+
     void Start() {
     }
 
     void Update() {
         player.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, Input.GetAxisRaw("Vertical") * speed);
         player.velocity.Normalize();
+    }
+
+    void OnCollisionEnter2D(Collision2D other) { // other es el otro objeto con el que colisiona
+        if (other.gameObject.tag == "Enemy" || other.gameObject.tag == "Flying_enemy") {
+            Destroy(other.gameObject);
+            health -= 1;
+            if (health == 0) {
+                Destroy(gameObject);
+            }
+        }
     }
 }
